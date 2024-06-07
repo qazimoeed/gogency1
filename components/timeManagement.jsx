4772b916e87c53c9dad0b103f4a802b5f21bc6c1
Qazi Moeed <qaziabdulmoeed5@gmail.com>
2024-06-07 11:24:37 +0500

    update

--- components/timeManagement.jsx
+++ components/timeManagement.jsx
@@ -7,27 +7,212 @@ export default function TimeManagement() {
 				<h1 className="firstheadtrack">Team &</h1>
 				<h1 className="teamSecHead">HR MANAGEMENT</h1>
 			</div>
+			<div className="timecardflex">
+				<ul className="uls inline">
+					<li>Team Performance</li>
+					<li>Attendance Record</li>
+					<li>Meeting Invites and Management</li>
+				</ul>
 
-			<ul className="uls inline">
-				<li>Team Performance</li>
-				<li>Attendance Record</li>
-				<li>Meeting Invites and Management</li>
-			</ul>
-
-			<ul className="uls inline">
-				<li>Team Performance</li>
-				<li>Attendance Record</li>
-				<li>Meeting Invites and Management</li>
-			</ul>
-
-			<div>
-				<div className="card">
+				<div className="timeManageCard">
 					<img src="./assets/timeCard1.svg" className="timeCardImage" />
 					<h1 className="cardHeader">John Doe</h1>
 					<p className="cardParagraph">CEO & Founder, Example</p>
+					<div>
+						<span className="cardbackcircle1 cardbackcircleText">84%</span>
+						<span className="cardbackcircle2 cardbackcircleText">85%</span>
+					</div>
+					<div>
+						<span>Month</span>
+						<span>Year</span>
+					</div>
 					<p>
-						<button className="cardbutton">Contact</button>
+						<button className="cardbutton">Profile Details</button>
 					</p>
+				</div>
+
+				<div className="timeManageCard">
+					<img src="./assets/timeCard2.svg" className="timeCardImage" />
+					<h1 className="cardHeader">John Doe</h1>
+					<p className="cardParagraph">CEO & Founder, Example</p>
+					<div>
+						<span className="cardbackcircle1 cardbackcircleText">84%</span>
+						<span className="cardbackcircle2 cardbackcircleText">85%</span>
+					</div>
+					<div>
+						<span>Month</span>
+						<span>Year</span>
+					</div>
+					<p>
+						<button className="cardbutton">Profile Details</button>
+					</p>
+				</div>
+
+				<div className="timeManageCard">
+					<img src="./assets/timeCard3.svg" className="timeCardImage" />
+					<h1 className="cardHeader">John Doe</h1>
+					<p className="cardParagraph">CEO & Founder, Example</p>
+					<div>
+						<span className="cardbackcircle1 cardbackcircleText">84%</span>
+						<span className="cardbackcircle2 cardbackcircleText">85%</span>
+					</div>
+					<div>
+						<span>Month</span>
+						<span>Year</span>
+					</div>
+					<p>
+						<button className="cardbutton">Profile Details</button>
+					</p>
+				</div>
+
+				<div className="timeManageCardBoundary">
+					<div className="innertrack1">
+						<div>
+							<h3>
+								<b>#3621245</b>
+							</h3>
+							<p>2m ago</p>
+						</div>
+						<div>
+							<h3>
+								<b>Arsalan Tariq</b>
+							</h3>
+							<p>arslantariqarsal@gmail.com</p>
+						</div>
+						<div>
+							<h3>
+								<b>John Doe</b>
+							</h3>
+							<p>johndoe@gmail.com</p>
+						</div>
+						<div>
+							<h3>
+								<b>LON</b>
+							</h3>
+							<p>London</p>
+						</div>
+						<div>
+							<h3>
+								<b>ACC</b>
+							</h3>
+							<p>accra</p>
+						</div>
+						<div>
+							<h3>
+								<b>13-10-2023</b>
+							</h3>
+							<p>
+								<b>13-10-2023</b>
+							</p>
+						</div>
+						<div>
+							<h3>
+								<b>1 Passenger</b>
+							</h3>
+							<p>$1500</p>
+						</div>
+						<div>
+							<img src="./assets/tracklogo.svg" className="trackimagecard" />
+						</div>
+					</div>
+					<div className="innertrack1">
+						<div>
+							<h3>
+								<b>#3621245</b>
+							</h3>
+							<p>2m ago</p>
+						</div>
+						<div>
+							<h3>
+								<b>Arsalan Tariq</b>
+							</h3>
+							<p>arslantariqarsal@gmail.com</p>
+						</div>
+						<div>
+							<h3>
+								<b>John Doe</b>
+							</h3>
+							<p>johndoe@gmail.com</p>
+						</div>
+						<div>
+							<h3>
+								<b>LON</b>
+							</h3>
+							<p>London</p>
+						</div>
+						<div>
+							<h3>
+								<b>ACC</b>
+							</h3>
+							<p>accra</p>
+						</div>
+						<div>
+							<h3>
+								<b>13-10-2023</b>
+							</h3>
+							<p>
+								<b>13-10-2023</b>
+							</p>
+						</div>
+						<div>
+							<h3>
+								<b>1 Passenger</b>
+							</h3>
+							<p>$1500</p>
+						</div>
+						<div>
+							<img src="./assets/tracklogo.svg" className="trackimagecard" />
+						</div>
+					</div>
+					<div className="innertrack1">
+						<div>
+							<h3>
+								<b>#3621245</b>
+							</h3>
+							<p>2m ago</p>
+						</div>
+						<div>
+							<h3>
+								<b>Arsalan Tariq</b>
+							</h3>
+							<p>arslantariqarsal@gmail.com</p>
+						</div>
+						<div>
+							<h3>
+								<b>John Doe</b>
+							</h3>
+							<p>johndoe@gmail.com</p>
+						</div>
+						<div>
+							<h3>
+								<b>LON</b>
+							</h3>
+							<p>London</p>
+						</div>
+						<div>
+							<h3>
+								<b>ACC</b>
+							</h3>
+							<p>accra</p>
+						</div>
+						<div>
+							<h3>
+								<b>13-10-2023</b>
+							</h3>
+							<p>
+								<b>13-10-2023</b>
+							</p>
+						</div>
+						<div>
+							<h3>
+								<b>1 Passenger</b>
+							</h3>
+							<p>$1500</p>
+						</div>
+						<div>
+							<img src="./assets/tracklogo.svg" className="trackimagecard" />
+						</div>
+					</div>
 				</div>
 			</div>
 		</>
