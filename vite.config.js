19f69c8f6174922c01373d5ad82b873937aec29c
Qazi Moeed <101875663+qazimoeed@users.noreply.github.com>
2024-06-25 16:41:47 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,12 @@
-
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react', 'react-dom', 'react-intersection-observer'],
-    },
-  },
-   resolve: {
+  resolve: {
     alias: {
-      'react-intersection-observer': 'react-intersection-observer', // Ensure this matches the package name in node_modules
+      'react-intersection-observer': 'react-intersection-observer',
     },
   },
 });
- 
- 
