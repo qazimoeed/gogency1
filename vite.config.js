c2de02264d9195d171cec996b28cce1470598198
Qazi Moeed <101875663+qazimoeed@users.noreply.github.com>
2024-06-25 14:31:37 +0500

    vite.config2.js

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,14 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
 	plugins: [react()],
 	build: {
+		outDir: 'dist',
 		rollupOptions: {
 			external: ["react-intersection-observer"],
+			input: '/src/main.jsx',
 		},
 	},
 	optimizeDeps: {
 	    include: ['react-intersection-observer'],
 	},
 })
-
-  
-
+ 
